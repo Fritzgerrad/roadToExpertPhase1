68ebd572156c93210baf2b19ebdccede58c0a335
Fritzgerrad <fritzgerrad.so@gmail.com>
2024-07-28 00:38:08 +0100

    Initialized the project

---
+++ HRbuddy/src/main/java/org/frz/hrbuddy/HRbuddyApplication.java
@@ -0,0 +1,13 @@
+package org.frz.hrbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRbuddyApplication.class, args);
+	}
+
+}
---
+++ HRbuddy/src/test/java/org/frz/hrbuddy/HRbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.frz.hrbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HRbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
