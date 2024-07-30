73f6211c0391021094a931f8bb7a82423ba810dd
Fritzgerrad <38596069+Fritzgerrad@users.noreply.github.com>
2024-07-30 22:51:37 +0100

    Revert "Initialation"

--- HRbuddy/src/main/java/org/frz/hrbuddy/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.frz.hrbuddy.repository;
-
-import org.frz.hrbuddy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUsername(String username);
-}
